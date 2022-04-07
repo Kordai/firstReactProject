86a42f9a8664f428e49da996e14158f66132056c
Kordai <40060837+Kordai@users.noreply.github.com>
2022-04-07 15:30:41 +0600

    new component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+const Header = ()=> {
+  return (   
+      <header className="App-header">
+        <h1>Первая страница React</h1>
+        <h2>Настройка Git</h2>
+      </header>   
+  );
+}
+
+export default Header;
