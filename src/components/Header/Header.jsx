b192554b3ee0afe7c67e466b45c3104cd7838fb5
Kordai <40060837+Kordai@users.noreply.github.com>
2022-04-07 16:07:57 +0600

    fiks

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Header.css';
 
-const Header = ()=> {
-  return (   
-      <header className="App-header">
-        <h1>Первая страница React</h1>
-        <h2>Настройка Git</h2>
-      </header>   
+const Header = () => {
+  return (
+    <header className="App-header">
+      <h1>Первая страница React</h1>
+      <h2>Настройка Git</h2>
+    </header>
   );
 }
 
