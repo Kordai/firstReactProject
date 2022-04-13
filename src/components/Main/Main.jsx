553e0e7df1d5c00e9367ea8691408450040dfa4d
Kordai <40060837+Kordai@users.noreply.github.com>
2022-04-13 15:04:51 +0600

    delete Section3

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 import s from './Main.module.css';
 import Section1 from './Sections/Section1/Section1';
 import Section2 from './Sections/Section2/Section2';
-import Section3 from './Sections/Section3/Section3';
 
 const Main = (props) => {
   return (
     <main className={s.m}>
       <Section1 state = {props.state.section1}/>
       <Section2/>
-      <Section3/>
     </main>
   );
 }
--- src/components/Main/Sections/Section2/Section2.jsx
+++ src/components/Main/Sections/Section2/Section2.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import s from './Section2.module.css';
+import Module2 from './Moduls/Module2/Module2'
 
 const Section2 = () => {
   return (
     <section className={s.s2}>
-      <h1>   Section2   </h1>
+      <Module2 />
+      <Module2 />
     </section>
   );
 }
