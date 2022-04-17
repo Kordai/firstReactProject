57a2cf593f9d80384e899cdfd03a2c78f8c37fc8
Kordai <40060837+Kordai@users.noreply.github.com>
2022-04-17 16:50:25 +0600

    add BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import Header from './components/Header/Header';
-
 import './App.css';
 import Nav from './components/Nav/Nav';
 import Main from './components/Main/Main';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 function App(props) {
   return (
-    <div className="App">
-      <Nav />
-      <Header />
-      <Main state = {props.state}/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Nav />
+        <Header />
+        <Main state={props.state} />
+      </div>
+    </BrowserRouter>
   );
 }
 
