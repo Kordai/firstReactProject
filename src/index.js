443eb7b3b71e3f98c1a9364ba5db19db4ff0e07c
Kordai <40060837+Kordai@users.noreply.github.com>
2022-04-18 11:45:23 +0600

    refactoring

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import state from './Data/state'
+import store from './Data/state'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App state={state} />
+    <App state={store.state} />
   </React.StrictMode>,
   document.getElementById('root')
 );
