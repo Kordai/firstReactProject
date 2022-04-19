4b1f2bce7564f0817771d5f49164e5f6456f0a6d
Kordai <40060837+Kordai@users.noreply.github.com>
2022-04-19 12:01:40 +0600

    refactoring Table

--- src/components/Main/Sections/Tabel/TableCell.jsx
+++ src/components/Main/Sections/Tabel/TableCell.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import './TableCell.css';
+
 
 const TableCell = () => {
   return (
     <div className="table__cell">
-      <input value="123" type="text"/>
+      <input value="123" type="text" className='imp'/>
     </div>
   )
 }
