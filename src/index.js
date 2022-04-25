6205a74ed963e0d247d80d82093ad656516493fd
Kordai <40060837+Kordai@users.noreply.github.com>
2022-04-25 15:53:14 +0600

    refactoring store

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import store from './Redux/state';
+import store from './Redux/store';
 
 let rerenderEntireTree = () => {
   ReactDOM.render(
