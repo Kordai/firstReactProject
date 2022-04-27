1f74d07525d8fcfb835c9fe0a417094450833e84
Kordai <40060837+Kordai@users.noreply.github.com>
2022-04-27 12:36:13 +0600

    refactoring table

--- src/components/Main/Sections/Section3/Moduls/Tabel/Table.jsx
+++ src/components/Main/Sections/Section3/Moduls/Tabel/Table.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import TableRow from './TableRow'
+import s from './Table.module.css';
 
-const Table = (props) => { 
-    return (       
-          <TableRow state={props.state}/>
-    )
-  }
+const Table = (props) => {
+      return (
+            <div className={s.t}>
+                  <TableRow state={props.state} />
+            </div>
+      )
+}
 
-  export default Table;
+export default Table;
