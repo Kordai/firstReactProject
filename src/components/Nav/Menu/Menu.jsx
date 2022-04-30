b35e2df073f2b56897cf8443ac48cf35b344fd27
Kordai <alex.trade@inbox.ru>
2022-04-30 21:11:13 +0600

    refactoring menu

--- src/components/Nav/Menu/Menu.jsx
+++ src/components/Nav/Menu/Menu.jsx
@@ -5,10 +5,11 @@ import {NavLink} from 'react-router-dom'
 const Menu = () => {
   return (
     <div className={s.m}>
-      <NavLink to="/" className={({ isActive }) => isActive ? s.active : undefined} >Панель управления</NavLink>
-      <NavLink to="/panelmoney" className={({ isActive }) => isActive ? s.active : undefined} >Оплаты</NavLink>
-      <NavLink to="/panelapplications" className={({ isActive }) => isActive ? s.active : undefined} >Заявки</NavLink>
-      <NavLink to="/panelpoints" className={({ isActive }) => isActive ? s.active : undefined} >Точки продаж</NavLink>
+      <NavLink to="/" className={({ isActive }) => isActive ? s.active : undefined} >Control panel</NavLink>
+      <NavLink to="/panelmoney" className={({ isActive }) => isActive ? s.active : undefined} >Payments</NavLink>
+      <NavLink to="/panelapplications" className={({ isActive }) => isActive ? s.active : undefined} >Applications</NavLink>
+      <NavLink to="/panelpoints" className={({ isActive }) => isActive ? s.active : undefined} >Points</NavLink>
+      <NavLink to="/panelusers" className={({ isActive }) => isActive ? s.active : undefined} >Users</NavLink>
     </div>
   );
 }
