e3263a5f4493625b607f77eead4ec53e96b198a3
Kordai <alex.trade@inbox.ru>
2022-04-30 19:40:24 +0600

    added new components for MainUsers

--- src/components/Main/MainUsers.jsx
+++ src/components/Main/MainUsers.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import s from './MainUsers.module.css';
+import Section4 from './Sections/Section4/Section4'
 
 const MainUsers = (props) => {  
 //   if (props.state.length === 0) {
 //     props.setNewUsers({
       
 //   });
-  }
+ // }
   return (
     <main className={s.u}>
-
+        <Section4/>
+        <Section4/>
+        <Section4/>
     </main>
   );
 }
---
+++ src/components/Main/Sections/Section4/Section4.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import s from './Section4.module.css';
+
+const Section4 = (props) => {  
+
+  return (
+    <main className={s.u}>
+
+    </main>
+  );
+}
+
+export default Section4;
