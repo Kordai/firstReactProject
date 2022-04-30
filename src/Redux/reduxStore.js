8e0dd2508b2eea546f5a9aec79407ee00446ad41
Kordai <alex.trade@inbox.ru>
2022-04-30 18:48:46 +0600

    refactoring reduxStore

--- src/Redux/reduxStore.js
+++ src/Redux/reduxStore.js
@@ -3,12 +3,14 @@ import section1Reducer from './section1Reducer';
 import section2Reducer from './section2Reducer';
 import section3Reducer from './section3Reducer';
 import applicationsReducer from './applicationsReducer';
+import usersReducer from './usersReducer';
 
 let reducers = combineReducers({
     section1: section1Reducer,
     section2: section2Reducer,
     section3: section3Reducer,
-    applications: applicationsReducer
+    applications: applicationsReducer,
+    users: usersReducer
 });
 let store = createStore(reducers);
 
