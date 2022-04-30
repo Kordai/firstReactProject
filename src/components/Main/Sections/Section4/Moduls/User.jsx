917eda9045bc25117d18d7a8ee8e5790ebf1ebde
Kordai <alex.trade@inbox.ru>
2022-04-30 21:02:20 +0600

    refactorin User

--- src/components/Main/Sections/Section4/Moduls/User.jsx
+++ src/components/Main/Sections/Section4/Moduls/User.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import s from './User.module.css';
+import userPhoto from '../../../../image/user.png'
 
-const User = (props) => {  
+const User = (props) => {
 
   return (
     <div className={s.u}>
-
+      <div className={s.info}>
+        <h2>Name</h2>
+        <h3>Phone</h3>
+        <h3>E-mail</h3>
+        <h3>City</h3>
+      </div>
+      <div className={s.img}>
+        <img src={userPhoto} ></img>
+      </div>
+      <div className={s.level}>
+        <h4>Level 254</h4>
+      </div>
     </div>
   );
 }
--- src/components/Main/Sections/Section4/Section4.jsx
+++ src/components/Main/Sections/Section4/Section4.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import s from './Section4.module.css';
+import User from './Moduls/User'
 
 const Section4 = (props) => {  
 
   return (
     <section className={s.u}>
-
+      <User/>
+      <User/>
+      <User/>
+      <User/>
+      <User/>
+      <User/>
+      <User/>
     </section>
   );
 }
