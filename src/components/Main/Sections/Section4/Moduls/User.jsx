44d5c0b59e19f022beb7361dc95d81d81c0d2bea
Kordai <alex.trade@inbox.ru>
2022-05-01 23:48:37 +0600

    refactoring users

--- src/components/Main/Sections/Section4/Moduls/User.jsx
+++ src/components/Main/Sections/Section4/Moduls/User.jsx
@@ -3,18 +3,17 @@ import s from './User.module.css';
 import userPhoto from '../../../../image/user.png'
 
 const User = (props) => {
-
   return (
     <div className={s.u}>
       <div className={s.info}>
-        <h3>{props.name}</h3>
-        <h4>{props.phone}</h4>
-        <h4>{props.email}</h4>
-        <h4>{props.city}</h4>
+        <h3>{props.state.name}</h3>
+        <h4>{props.state.phone}</h4>
+        <h4>{props.state.email}</h4>
+        <h4>{props.state.city}</h4>
       </div>      
-        <img src={userPhoto} alt={props.name} className={s.img}></img>      
+        <img src={userPhoto} alt={props.state.name} className={s.img}></img>      
       <div className={s.level}>
-        <h5>Level {props.level} </h5>
+        <h5>Level {props.state.level} </h5>
       </div>
     </div>
   );
--- src/components/Main/Sections/Section4/Section4.jsx
+++ src/components/Main/Sections/Section4/Section4.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import s from './Section4.module.css';
 import User from './Moduls/User'
 
-const Section4 = (props) => {  
+const Section4 = (props) => {
+
   let userPage = props.state.map(
-    val => {
-      <User state = {val} key = {val.id} />
-    }
+    val => <User state={val} />   
   );
+
   return (
     <section className={s.u}>
-     {userPage}
+      {userPage}
     </section>
   );
 }
