59463b2e94b0b38e300bdb7bfaf43df5b77092af
Kordai <alex.trade@inbox.ru>
2022-05-07 12:09:01 +0600

    refactoring component navBar

--- src/components/Nav/Menu/Menu.jsx
+++ src/components/Nav/Menu/Menu.jsx
@@ -5,11 +5,11 @@ import {NavLink} from 'react-router-dom'
 const Menu = () => {
   return (
     <div className={s.m}>
-      <NavLink to="/" className={({ isActive }) => isActive ? s.active : undefined} >Control panel</NavLink>
-      <NavLink to="/panelmoney" className={({ isActive }) => isActive ? s.active : undefined} >Payments</NavLink>
-      <NavLink to="/panelapplications" className={({ isActive }) => isActive ? s.active : undefined} >Applications</NavLink>
-      <NavLink to="/panelpoints" className={({ isActive }) => isActive ? s.active : undefined} >Points</NavLink>
-      <NavLink to="/panelusers" className={({ isActive }) => isActive ? s.active : undefined} >Users</NavLink>
+      <NavLink to="/" className={({ isActive }) => isActive ? s.active : s.decarationNone} >Control panel</NavLink>
+      <NavLink to="/panelmoney" className={({ isActive }) => isActive ? s.active : s.decarationNone} >Payments</NavLink>
+      <NavLink to="/panelapplications" className={({ isActive }) => isActive ? s.active : s.decarationNone} >Applications</NavLink>
+      <NavLink to="/panelpoints" className={({ isActive }) => isActive ? s.active : s.decarationNone} >Points</NavLink>
+      <NavLink to="/panelusers" className={({ isActive }) => isActive ? s.active : s.decarationNone} >Users</NavLink>
     </div>
   );
 }
