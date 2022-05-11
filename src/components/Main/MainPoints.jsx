e08e49bcd050f8246c0424fd94cb371567245aa7
Kordai <40060837+Kordai@users.noreply.github.com>
2022-05-11 17:32:07 +0600

    refactoring

--- src/components/Main/MainPoints.jsx
+++ src/components/Main/MainPoints.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import s from './MainPoints.module.css';
 
 const MainPoints = (props) => {
+
   return (
     <main className={s.m}>
       MainPoints
+      <div>
+        <button>Set Cookie</button>
+      </div>
+      <div>
+        <button >Get Cookie</button>
+      </div>
     </main>
-  );
+  )
 }
 
 export default MainPoints;
