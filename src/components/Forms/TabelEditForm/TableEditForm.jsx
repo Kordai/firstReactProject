98f9849ad6722359dd5da4ec9f9fea83aab4ba83
Kordai <alex.trade@inbox.ru>
2022-05-15 15:06:22 +0600

    refactoring

--- src/components/Forms/TabelEditForm/TableEditForm.jsx
+++ src/components/Forms/TabelEditForm/TableEditForm.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { reduxForm } from 'redux-form';
 import s from './TableEditForm.module.css';
 
 const TableEditForm = (props) => {
 
+    const name = props.formName;
+
     return <>
         <div ref={props.form} className={s.formPopup}>
-            <h1>Add {props.formName}</h1>
+            <h1>Add {name}</h1>
             {props.inputComponent}
             <button onClick={props.sendForm} className={s.btn}>Send</button>
             <button onClick={props.closeForm} className={s.cancel}>Close</button>
@@ -14,4 +17,6 @@ const TableEditForm = (props) => {
 
 }
 
-export default TableEditForm;
+export default reduxForm({
+    form: name
+})(TableEditForm)
