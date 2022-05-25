8672d2faa888e8d402fa7476b86c3d542a1fda1b
Kordai <alex.trade@inbox.ru>
2022-05-25 22:55:11 +0600

    refactoring UserProfile

--- src/components/Main/Sections/Section4/Moduls/ModuleTextLabel.jsx
+++ src/components/Main/Sections/Section4/Moduls/ModuleTextLabel.jsx
@@ -4,7 +4,7 @@ import s from './ModuleTextLabel.module.css';
 const ModuleTextLabel = (props) => {
     return <div className={s.data}>
     <span className={s.tegdata}>{props.teg}</span>
-    <span className={s.text}> {props.text}</span>
+    <span className={s.text}> {props.text}</span>    
 </div>
 }
 
--- src/components/Main/Sections/Section4/Profile/UserProfile.jsx
+++ src/components/Main/Sections/Section4/Profile/UserProfile.jsx
@@ -4,29 +4,59 @@ import userPhoto from '../../../../image/user.png'
 import ModuleTextLabel from '../Moduls/ModuleTextLabel';
 
 const UserProfile = () => {
-    return <main className={s.profile}>
-        <div className={s.user}>
-            <span className={s.teguser}>User info</span>
-            <img src={userPhoto} alt="User" className={s.img}></img>
-            <div className={s.login}>
-                <h4>User login: LOGIN</h4>
-                <h4>User team: TEAM</h4>
+
+    let userAttributes =
+        [
+            "First name",
+            "Last name",
+            "Address",
+            "City",
+            "Namber phone",
+            "E-mail"
+        ]
+
+    let stockAttributes =
+        [
+            "Coffee",
+            "Milk",
+            "Cups",
+            "Sugar"
+        ]
+
+    let userInfoModule = userAttributes.map(val => <ModuleTextLabel teg={val} text={val} key={val} />)
+    let stockModule = stockAttributes.map(val => <ModuleTextLabel teg={val} text={val} key={val} />)
+
+    return <>
+        <main className={s.profile}>
+            <div className={s.user}>
+                <span className={s.teguser}>User info</span>
+                <img src={userPhoto} alt="User" className={s.img}></img>
+                <div className={s.login}>
+                    <h4>User login: LOGIN</h4>
+                    <h4>User team: TEAM</h4>
+                </div>
+                {userInfoModule}
+                <button type='button' className={s.updateBTN}>Update</button>
+                <button type='button' className={s.deleteBTN}>Delete</button>
             </div>
-            <ModuleTextLabel teg = "First name" text = "1231321321"/>
-            <ModuleTextLabel teg = "Last name" text = "1231321321"/>
-            <ModuleTextLabel teg = "Address" text = "1231321321"/>
-            <ModuleTextLabel teg = "City" text = "1231321321"/>
-            <ModuleTextLabel teg = "Namber phone" text = "1231321321"/>
-            <ModuleTextLabel teg = "E-mail" text = "1231321321"/>
+            <div className={s.applications}>
+                <span className={s.teguser}>Applications</span>
+            </div>
+            <div className={s.info}>
+                <span className={s.teguser}>Info</span>
+            </div>
+            {/* //Coffee	Milk	Cups	Sugar */}
+            <div className={s.stock}>
+                <span className={s.teguser}>Stock</span>
+                <div className={s.stockModule}>
+                    {stockModule}
+                </div>
 
-            <button type='button' className={s.updateBTN}>Update</button>
-            <button type='button' className={s.deleteBTN}>Delete</button>
-        </div>
-        <div className={s.applications}></div>
-        <div className={s.info}></div>
-        <div className={s.stock}></div>
-    </main>
+            </div>
 
+
+        </main>
+    </>
 }
 
 export default UserProfile
