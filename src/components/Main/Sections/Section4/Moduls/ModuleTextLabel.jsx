40a63c9a8c28d688f5ada5335d50286d5d0120e2
Kordai <alex.trade@inbox.ru>
2022-05-25 22:04:21 +0600

    created new module for User ProFile

---
+++ src/components/Main/Sections/Section4/Moduls/ModuleTextLabel.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import s from './ModuleTextLabel.module.css';
+
+const ModuleTextLabel = (props) => {
+    return <div className={s.data}>
+    <span className={s.tegdata}>{props.teg}</span>
+    <span className={s.text}> {props.text}</span>
+</div>
+}
+
+export default ModuleTextLabel
--- src/components/Main/Sections/Section4/Profile/UserProfile.jsx
+++ src/components/Main/Sections/Section4/Profile/UserProfile.jsx
@@ -1,45 +1,26 @@
 import React from 'react';
 import s from './UserProfile.module.css';
 import userPhoto from '../../../../image/user.png'
+import ModuleTextLabel from '../Moduls/ModuleTextLabel';
 
 const UserProfile = () => {
     return <main className={s.profile}>
         <div className={s.user}>
             <span className={s.teguser}>User info</span>
-            <img src={userPhoto} alt="User Photo" className={s.img}></img>
+            <img src={userPhoto} alt="User" className={s.img}></img>
             <div className={s.login}>
                 <h4>User login: LOGIN</h4>
                 <h4>User team: TEAM</h4>
             </div>
-            <div className={s.data}>
-                <span className={s.tegdata}>First name</span>
-                <span className={s.text}> 1231321321</span>
-            </div>
-            <div className={s.data}>
-                <span className={s.tegdata}>Last name</span>
-                <span className={s.text}> 1231321321</span>
-            </div>
-            <div className={s.data}>
-                <span className={s.tegdata}>Address</span>
-                <span className={s.text}> 1231321321</span>
-            </div>
-            <div className={s.data}>
-                <span className={s.tegdata}>City</span>
-                <span className={s.text}> 1231321321</span>
-            </div>
-            <div className={s.data}>
-                <span className={s.tegdata}>Namber phone</span>
-                <span className={s.text}> 1231321321</span>
-            </div>
-            <div className={s.data}>
-                <span className={s.tegdata}>E-mail</span>
-                <span className={s.text}> 1231321321</span>
-            </div>
-            <div className={s.footer}>
-            <span className={s.rating}>Rating</span>
-            <span className={s.date}>25-02-2022</span>
-            </div>
-            
+            <ModuleTextLabel teg = "First name" text = "1231321321"/>
+            <ModuleTextLabel teg = "Last name" text = "1231321321"/>
+            <ModuleTextLabel teg = "Address" text = "1231321321"/>
+            <ModuleTextLabel teg = "City" text = "1231321321"/>
+            <ModuleTextLabel teg = "Namber phone" text = "1231321321"/>
+            <ModuleTextLabel teg = "E-mail" text = "1231321321"/>
+
+            <button type='button' className={s.updateBTN}>Update</button>
+            <button type='button' className={s.deleteBTN}>Delete</button>
         </div>
         <div className={s.applications}></div>
         <div className={s.info}></div>
