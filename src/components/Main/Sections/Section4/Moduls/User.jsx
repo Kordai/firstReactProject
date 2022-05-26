ea1f7e53a3c16450b514867fb6fa9ea4c000b3c9
Kordai <40060837+Kordai@users.noreply.github.com>
2022-05-26 12:46:27 +0600

    refactoring UI User

--- src/components/Main/Sections/Section4/Moduls/User.jsx
+++ src/components/Main/Sections/Section4/Moduls/User.jsx
@@ -6,31 +6,32 @@ import { NavLink } from 'react-router-dom';
 const User = (props) => {
 
   const openProfileUser = (id) => {
-    
+
     console.log(id)
-    
+
   }
 
   return (
     <NavLink className={s.noDecoration} to={"/panelusers/userprofile/" + props.state.id}>
-    <div onDoubleClick={() => openProfileUser(props.state)} className={s.u}>
-      <div className={s.info}>
-        <div>
-          <h3>{props.state.firstName}</h3>
-          <h3>{props.state.lastName}</h3>
-          <h5>{props.state.phone}</h5>
-        </div>
-        <img src={userPhoto} alt={props.state.name} className={s.img}></img>
-      </div>
-      <div className={s.contact}>
-        <h5>{props.state.email}</h5>
-        <h5>{props.state.city}</h5>
-      </div>
+      <div onDoubleClick={() => openProfileUser(props.state)} className={s.u}>
+        <div className={s.info}>
+          <div>
+            <h3>{props.state.firstName}</h3>
+            <h3>{props.state.lastName}</h3>
 
-      <div className={s.level}>
-        <h6>Level {props.state.level} </h6>
+          </div>
+          <img src={userPhoto} alt={props.state.name} className={s.img}></img>
+        </div>
+        <div className={s.contact}>
+          <h5>{props.state.phone}</h5>
+          <h5>{props.state.email}</h5>
+          <h5>{props.state.city}</h5>
+        </div>
+
+        <div className={s.level}>
+          <h6>Level {props.state.level} </h6>
+        </div>
       </div>
-    </div>
     </NavLink>
   );
 }
