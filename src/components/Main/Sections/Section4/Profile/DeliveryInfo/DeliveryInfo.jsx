46c2f21cc08ba4e9a3a91033544edd4349c9c1a9
Kordai <40060837+Kordai@users.noreply.github.com>
2022-05-31 13:13:48 +0600

    added to chart DeliveryInfo all attributes

--- src/components/Main/Sections/Section4/Profile/DeliveryInfo/DeliveryInfo.jsx
+++ src/components/Main/Sections/Section4/Profile/DeliveryInfo/DeliveryInfo.jsx
@@ -4,18 +4,18 @@ import ModuleTextLabel from '../Moduls/ModuleTextLabel';
 import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, CartesianGrid } from 'recharts';
 
 const data = [
-    { name: 'January', Coffee: 400, Milk: 1400},
-    { name: 'February', Coffee: 300, Milk: 1600 },
-    { name: 'March', Coffee: 250, Milk: 1800 },
-    { name: 'April', Coffee: 600, Milk: 2000 },
-    { name: 'May', Coffee: 550, Milk: 2200 },
-    { name: 'June', Coffee: 500, Milk: 2400 },
-    { name: 'July', Coffee: 400, Milk: 1400},
-    { name: 'August', Coffee: 300, Milk: 1600 },
-    { name: 'September', Coffee: 250, Milk: 1800 },
-    { name: 'October', Coffee: 600, Milk: 2000 },
-    { name: 'November', Coffee: 550, Milk: 2200 },
-    { name: 'December', Coffee: 500, Milk: 2400 }
+    { name: 'January', Coffee: 400, Milk: 1400, Cups: 400, Sugar: 1400},
+    { name: 'February', Coffee: 300, Milk: 1600, Cups: 400, Sugar: 1400 },
+    { name: 'March', Coffee: 250, Milk: 1800, Cups: 400, Sugar: 1400 },
+    { name: 'April', Coffee: 600, Milk: 2000, Cups: 400, Sugar: 1400 },
+    { name: 'May', Coffee: 550, Milk: 2200, Cups: 400, Sugar: 1400 },
+    { name: 'June', Coffee: 500, Milk: 2400, Cups: 400, Sugar: 1400 },
+    { name: 'July', Coffee: 400, Milk: 1400, Cups: 400, Sugar: 1400},
+    { name: 'August', Coffee: 300, Milk: 1600, Cups: 400, Sugar: 1400 },
+    { name: 'September', Coffee: 250, Milk: 1800, Cups: 400, Sugar: 1400 },
+    { name: 'October', Coffee: 600, Milk: 2000, Cups: 400, Sugar: 1400 },
+    { name: 'November', Coffee: 550, Milk: 2200, Cups: 400, Sugar: 1400 },
+    { name: 'December', Coffee: 500, Milk: 2400, Cups: 400, Sugar: 1400 }
 ];
 
 const DeliveryInfo = (props) => {
@@ -61,8 +61,10 @@ const DeliveryInfo = (props) => {
                     <Tooltip itemStyle={{ color: '#282c34' }} contentStyle={{ backgroundColor: '#727e94', color: '#282c34' }}  cursor={{fill: '#727e94'}}/>
                     <Legend  wrapperStyle={{ bottom: 0, right: 0, backgroundColor: '#727e94', border: '1px solid #d5d5d5', borderRadius: 3, lineHeight: '40px' }} />
                     <CartesianGrid stroke="#00ffff" strokeDasharray="5 5" />
-                    <Bar dataKey="Coffee" fill="#00ffff" barSize={10} />
-                    <Bar dataKey="Milk" fill="#00ffaa" barSize={10} />
+                    <Bar dataKey="Coffee" fill="#00ffff" barSize={7} />
+                    <Bar dataKey="Milk" fill="#00ffaa" barSize={7} />
+                    <Bar dataKey="Cups" fill="#f56f6f" barSize={7} />
+                    <Bar dataKey="Sugar" fill="#6fb4f5" barSize={7} />
                 </BarChart>
             </div>
 
