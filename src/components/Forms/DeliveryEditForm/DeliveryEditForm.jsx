75392b3e2e16e7a652fb7dfa20ca4454ca1cc59c
Kordai <40060837+Kordai@users.noreply.github.com>
2022-06-01 16:45:29 +0600

    added new form for Delivery table

---
+++ src/components/Forms/DeliveryEditForm/DeliveryEditForm.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { Field, reduxForm } from 'redux-form';
+import s from './TableEditForm.module.css';
+
+const TableEditForm = (props) => {
+
+    const name = props.formName;
+
+    let inputComponent = props.inputVal.map(
+        (val) => {
+            return (
+                <Field
+                    component="input"
+                    placeholder={val}
+                    name={val}
+                    key={props.inputVal.indexOf(val)}
+                />
+            )
+        }
+    );
+
+    return <>
+        <form onSubmit={props.handleSubmit} className={props.activeForm ? s.formPopup : s.formPopupNone}>
+            <h1>{props.nameAction} {name}</h1>
+            <label className={props.nameAction === "Add" ? s.noneLabel : s.activeLabel} name={"Id"} >ID: {props.initialValues.Id}</label>
+            {inputComponent}
+            <button className={s.btn}>{props.nameAction === "Add" ? "Submit" : "Update"}</button>
+            <button onClick={() => props.onDeletePayment(props.initialValues.Id)} className={props.nameAction === "Add" ? s.noneLabel : s.deleteButton} type="button">Delete</button>
+            <button onClick={props.closeForm} className={s.cancel} type="button">Close</button>
+        </form>
+    </>
+
+}
+
+const mapStateToProps = (state, ownProps) => ({
+    form: ownProps.formName,
+    enableReinitialize: true
+});
+
+export default compose(
+    connect(mapStateToProps),
+    reduxForm(),
+)(TableEditForm);
