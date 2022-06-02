ebb76ec9695437a341e48e7dcb3af72fa3f90334
Kordai <40060837+Kordai@users.noreply.github.com>
2022-06-02 10:56:29 +0600

    refactoring form Login

--- src/components/Forms/LoginForm/LoginForm.jsx
+++ src/components/Forms/LoginForm/LoginForm.jsx
@@ -1,45 +1,33 @@
 import React from 'react';
-import s from './UserForm.module.css';
-import { toggleBeForm } from '../../../Redux/usersReducer';
+import s from './LoginForm.module.css';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { Field, reduxForm } from 'redux-form';
 
-const UserForm = (props) => {
+const LoginForm = (props) => {
 
     return <>
         <form onSubmit={props.handleSubmit} className={props.state.activeForm ? s.visible : s.invisible}>
             <div>
-                <Field component="label" name="formName">{props.formName} User</Field>
+                <Field component="label" name="formName">Login</Field>
             </div>
             <br />
-            <Field component="input" placeholder="First name" name="firstName" />
-            <Field component="input" placeholder="Last name" name="lastName" />
-            <Field component="input" placeholder="Address" name="address" />
-            <Field component="input" placeholder="City" name="city" />
-            <Field component="input" placeholder="Namber phone" name="phone" />
-            <Field component="input" placeholder="E-mail" name="email" />
-            <Field component="select" name="team">
-                <option >Select a team</option>
-                <option value="Service">Service</option>
-                <option value="Meneger">Meneger</option>
-                <option value="Admin">Admin</option>
-            </Field>
-            <Field component="input" placeholder="Login" name="login" />
-            <Field component="input" placeholder="Password" name="password" />
-            <button>Submit</button>
-            <button onClick={() => props.toggleBeForm(false)} type='button'>Close</button>
+            <Field component="input" placeholder="Login or e-mail" name="login" />
+            <Field component="input" placeholder="Password" name="password" type="password" />
+            
+            <button>Login</button>
+            <button type='button'>Close</button>
         </form>
     </>
 }
 //
 const mapStateToProps = (state) => ({
-    form: "UserForm",
-    state: state.users,
+    form: "LoginForm",
+    state: state.auth,
     enableReinitialize: true
 });
 
 export default compose(
-    connect(mapStateToProps, { toggleBeForm }),
+    connect(mapStateToProps, {  }),
     reduxForm(),
-)(UserForm);
+)(LoginForm);
