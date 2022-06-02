c1cf15e4c83e15d35a8453e96a1c8544a084b6ba
Kordai <40060837+Kordai@users.noreply.github.com>
2022-06-02 11:03:50 +0600

    created authReducer

---
+++ src/Redux/authReducer.js
@@ -0,0 +1,137 @@
+import ConnectToServer from "../APIConnect/ConnectToServer";
+
+// const SET_USERS = 'SET_USERS';
+// const TOGGLE_IS_FETCHING = 'TOGGLE_IS_FETCHING';
+// const TOGGLE_BE_FORM = 'TOGGLE_BE_FORM';
+// const SET_USER_PROFILE_INFO = 'SET_USER_PROFILE_INFO';
+
+//Started props
+let initialState = {
+    user: {
+        id: null,
+        photo: null,
+        login: null,
+        password: null,
+        firstName: null,
+        lastName: null
+    },
+    isFetching: false,
+    activeForm: false,
+    userProfileInfo: {}
+};
+
+//Reducers functions
+const authReducer = (state = initialState, action) => {
+    switch (action.type) {
+        // case SET_USERS:
+        //     return {
+        //         ...state,
+        //         users: action.users
+        //     }
+        // case TOGGLE_IS_FETCHING:
+        //     return {
+        //         ...state,
+        //         isFetching: action.isFetching
+        //     }
+        // case TOGGLE_BE_FORM:
+        //     return {
+        //         ...state,
+        //         activeForm: action.activeForm
+        //     }
+        // case SET_USER_PROFILE_INFO:
+        //     return {
+        //         ...state,
+        //         userProfileInfo: action.userProfileInfo
+        //     }
+        default:
+            return state;
+    }
+}
+
+//Action Creators functions
+// const setNewUsers = (data) => {
+//     return {
+//         type: SET_USERS,
+//         users: data
+//     }
+// }
+
+// const setUserProfileInfo = (userProfileInfo) => {
+//     return {
+//         type: SET_USER_PROFILE_INFO,
+//         userProfileInfo
+//     }
+// }
+
+// const toggleIsFetching = (isFetching) => {
+//     return {
+//         type: TOGGLE_IS_FETCHING,
+//         isFetching
+//     }
+// }
+
+// export const toggleBeForm = (activeForm) => {
+//     return {
+//         type: TOGGLE_BE_FORM,
+//         activeForm
+//     }
+// }
+
+//Thunk functions
+// export const getUsers = () => {
+//     return (dispatch) => {
+//         dispatch(toggleIsFetching(true));
+//         ConnectToServer.getUsers().then(data => {
+//             dispatch(setNewUsers(data.users))
+//             dispatch(toggleIsFetching(false))
+//         });
+//     }
+// }
+
+// export const newUser = (user) => {
+//     return (dispatch) => {
+//         ConnectToServer.addNewUser(user).then(data => {
+//             if (data.success === 1) {
+//                 dispatch(getUsers())
+//                 dispatch(toggleBeForm(false))
+//             }
+//         });
+//     }
+// }
+
+// export const getUserProfileInfo = (id) => {
+//     return (dispatch) => {
+//         dispatch(toggleIsFetching(true));        
+//         ConnectToServer.getUser(id).then(data => {
+//             if (data.success === 1) {
+//                 dispatch(setUserProfileInfo(data.user))
+//                 dispatch(toggleIsFetching(false))
+//             }
+//         });
+//     }
+// }
+
+// export const updateUser = (user) => {
+//     return (dispatch) => {
+//         ConnectToServer.putUser(user).then(data => {
+//             if (data.success === 1) {
+//                 dispatch(getUserProfileInfo(user.id))
+//                 dispatch(toggleBeForm(false))
+//             }
+//         });
+//     }
+// }
+
+// export const deleteUser = (id) => {
+//     return (dispatch) => {
+//         dispatch(toggleIsFetching(true));        
+//         ConnectToServer.deleteUser(id).then(data => {
+//             if (data.success === 1) {      
+//                 dispatch(getUsers())           
+//                 dispatch(toggleIsFetching(false))
+//             }
+//         });
+//     }
+// }
+
+export default authReducer;
