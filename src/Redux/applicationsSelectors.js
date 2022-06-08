ef46cf295262bf3152e2b89d8870b78b9537cda2
Kordai <alex.trade@inbox.ru>
2022-06-08 21:35:18 +0600

    refactoring

--- src/Redux/applicationsSelectors.js
+++ src/Redux/applicationsSelectors.js
@@ -1,1 +1,10 @@
+import { createSelector } from "reselect"
+import { getAuthUserId, getAuthUserTeam } from "./authSelectors"
+
 export const getApplications = state => state.applications
+
+export const getDelideryActiveUser = createSelector(getApplications, getAuthUserTeam, getAuthUserId, 
+    (applications, team, id) => {
+        //if (team === "Service") return applications.filter((a) => a.)
+        return null
+    })
