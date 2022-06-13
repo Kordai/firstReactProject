bad026a9c34e0b34e63d2e2a8396a82badfe8fdf
Kordai <40060837+Kordai@users.noreply.github.com>
2022-06-13 13:17:04 +0600

    refactoring

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,7 +4,7 @@ import s from './Main.module.css';
 import Section1Container from './Sections/Section1/Section1Container';
 import Section2Container from './Sections/Section2/Section2Container';
 
-const Main = (props) => {
+const Main = () => {
   const navigation = useNavigate();
   return (
     <main className={s.m}>
