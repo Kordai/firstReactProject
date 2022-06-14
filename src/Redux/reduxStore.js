f794c3727c300156676b8bdf2505145f6590a88f
Kordai <40060837+Kordai@users.noreply.github.com>
2022-06-14 11:45:29 +0600

    added REDUX_DEVTOOLS settings

--- src/Redux/reduxStore.js
+++ src/Redux/reduxStore.js
@@ -1,4 +1,4 @@
-import  {applyMiddleware, combineReducers, createStore} from 'redux';
+import  {applyMiddleware, combineReducers, compose, createStore} from 'redux';
 import section1Reducer from './section1Reducer';
 import section2Reducer from './section2Reducer';
 import section3Reducer from './section3Reducer';
@@ -17,6 +17,9 @@ let reducers = combineReducers({
     auth: authReducer,
     form: formReducer
 });
-let store = createStore(reducers, applyMiddleware(thunkMiddleware));
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware)));
+//let store = createStore(reducers, applyMiddleware(thunkMiddleware));
 window.store = store;
 export default store;
